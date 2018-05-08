ed54c6153401ca6c9f395af3e27c4ce9213b2336
Eric Wang <ejwang@stanford.edu>
2018-05-08 00:50:19 -0700

    basic lda skeleton

--- Source/lda.py
+++ Source/lda.py
@@ -3,31 +3,68 @@ import sys
 import objects
 import string
 import re
+import enchant
 from bs4 import BeautifulSoup
 from sklearn.decomposition import LatentDirichletAllocation
 from collections import Counter
 from sklearn.feature_extraction import DictVectorizer
 
-pickles = ['194919.pkl', '195561.pkl', '11.pkl']
-# encoded with py2?
+import os
+
+data_dir = '../Data/nd'
+
+filenames = os.listdir(data_dir)
+filepaths = [os.path.join(data_dir, filename) for filename in filenames]
+num_files = len(filenames)
 
 count_dicts = []
-for pkl in pickles:
-    pickle_file = open('./' + pkl, "rb")
-    unpickler = pickle.Unpickler(pickle_file)
-    j = unpickler.load()
-    soup = BeautifulSoup(j.html, 'lxml')
-    text = soup.get_text()
-    # text = text.translate(None, string.punctuation)
-    text = re.sub(r'[^\w\s]','', text)
-    text = text.lower()
-    words = text.split()
-    counts = Counter(words)
-    count_dicts.append(counts)
 
-v = DictVectorizer(sparse = False)
+'''
+for i, pkl in enumerate(filepaths):
+    with open('./' + pkl, "rb") as pickle_file:
+        unpickler = pickle.Unpickler(pickle_file)
+        j = unpickler.load()
+        soup = BeautifulSoup(j.html, 'lxml')
+        text = soup.get_text()
+        # text = text.translate(None, string.punctuation)
+        text = re.sub(r'[^\w\s]','', text)
+        text = text.lower()
+        words = text.split()
+        words = [word for word in words if word.isalpha()]
+        counts = Counter(words)
+        count_dicts.append(counts)
+    print "processed " + pkl + ": " + str(i) + "/" + str(num_files)
+'''
+
+with open('../Data/count_dicts_nd.py', 'rb') as wc_file:
+    unpickler = pickle.Unpickler(wc_file)
+    count_dicts = unpickler.load()
+
+print('unpickler loaded')
+w = enchant.Dict("en_US")
+
+for i, count_dict in enumerate(count_dicts):
+    for key in count_dict.keys():
+        if not w.check(key) or not key.isalpha():
+            del count_dict[key]
+
+v = DictVectorizer(sparse = True)
 X = v.fit_transform(count_dicts)
+fnames = v.get_feature_names()
+with open('../Data/feature_names.pkl', 'wb') as fn_file:
+    pickle.dump(fnames, fn_file)
+
+
+
+'''
+with open('../Data/tfidf_matrix.pkl', 'rb') as wc_file:
+    unpickler = pickle.Unpickler(wc_file)
+    X = unpickler.load()
+
+with open('../Data/feature_names.pkl', 'rb') as feature_names_file:
+    unpickler = pickle.Unpickler(feature_names_file)
+    fn = unpickler.load()
 
 lda = LatentDirichletAllocation(n_components=5, learning_method = 'online')
-print(lda.fit_transform(X))
-
+ft = lda.fit_transform(X)
+'''
